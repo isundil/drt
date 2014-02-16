0dd14d67108dfde9031fafeefbcb400acf5db012
Thibault Barre-villeneuve <isundill@gmail.com>
2014-02-17 02:11:57 +0300

    SAuth message

---
+++ server/src/kernel/network/NetworkPacket.cpp
@@ -0,0 +1,203 @@
+#include <map>
+#include "NetworkPacket.hpp"
+
+using namespace drt::network;
+
+ANetworkPacket *ANetworkPacket::fromSocket(char code, FILE *socket)
+{
+	std::map<short, constructorFnc> ctors;
+
+	ctors[ 0] = SAuth::create;
+	ctors[ 1] = CAuth::create;
+	ctors[ 2] = Welcome::create;
+	ctors[ 3] = IdCh::create;
+	ctors[ 4] = Relog::create;
+	ctors[ 5] = Confirm::create;
+	ctors[ 6] = Quit::create;
+	ctors[ 7] = Netsplit::create;
+	ctors[ 8] = NewJob::create;
+	ctors[ 9] = EndJob::create;
+	ctors[10] = Ready::create;
+	ctors[11] = Proc::create;
+	ctors[12] = Calc::create;
+	ctors[13] = Result::create;
+	ctors[14] = CompilFail::create;
+
+	auto f = ctors.find(code);
+	if (f == ctors.end())
+		return nullptr;
+	return (*f).second(socket);
+}
+
+SAuth::SAuth(unsigned short _id): id(_id)
+{ }
+
+ANetworkPacket * SAuth::create(FILE * socket)
+{
+	unsigned short id;
+
+	fread(&id, sizeof(id), 1, socket);
+	return new SAuth(id);
+}
+
+ANetworkPacket * CAuth::create(FILE * socket)
+{
+	return new CAuth();
+}
+
+ANetworkPacket * Welcome::create(FILE * socket)
+{
+	return new Welcome();
+}
+
+ANetworkPacket * IdCh::create(FILE * socket)
+{
+	return new IdCh();
+}
+
+ANetworkPacket * Relog::create(FILE * socket)
+{
+	return new Relog();
+}
+
+ANetworkPacket * Confirm::create(FILE * socket)
+{
+	return new Confirm();
+}
+
+ANetworkPacket * Quit::create(FILE * socket)
+{
+	return new Quit();
+}
+
+ANetworkPacket * Netsplit::create(FILE * socket)
+{
+	return new Netsplit();
+}
+
+ANetworkPacket * NewJob::create(FILE * socket)
+{
+	return new NewJob();
+}
+
+ANetworkPacket * EndJob::create(FILE * socket)
+{
+	return new EndJob();
+}
+
+ANetworkPacket * Ready::create(FILE * socket)
+{
+	return new Ready();
+}
+
+ANetworkPacket * Proc::create(FILE * socket)
+{
+	return new Proc();
+}
+
+ANetworkPacket * Calc::create(FILE * socket)
+{
+	return new Calc();
+}
+
+ANetworkPacket * Result::create(FILE * socket)
+{
+	return new Result();
+}
+
+ANetworkPacket * CompilFail::create(FILE * socket)
+{
+	return new CompilFail();
+}
+
+std::stringstream * SAuth::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = new std::stringstream();
+	*ss << '\0' << (unsigned short) id;
+	return ss;
+}
+
+std::stringstream * CAuth::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Welcome::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * IdCh::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Relog::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Confirm::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Quit::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Netsplit::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * NewJob::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * EndJob::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Ready::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Proc::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Calc::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * Result::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
+std::stringstream * CompilFail::getStream(size_t *buflen) const
+{
+	std::stringstream *ss = nullptr;
+	return ss;
+}
+
---
+++ server/src/kernel/network/NetworkPacket.hpp
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <sstream>
+#include <stdio.h>
+
+#define NETWORK_MAXCODE  *create(14)
+
+
+namespace drt
+{
+namespace network
+{
+
+class ANetworkPacket
+{
+	public:
+		static ANetworkPacket *fromSocket (char code, FILE *socket);
+
+		virtual std::stringstream *getStream(size_t *buflen) const =0;
+
+	protected:
+};
+
+typedef ANetworkPacket  *(*constructorFnc) (FILE *);
+
+class SAuth: public ANetworkPacket
+{
+	public:
+		SAuth(unsigned short id);
+
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+
+	private:
+		SAuth();
+
+	private:
+		unsigned short id;
+};
+
+class CAuth: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Welcome: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class IdCh: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Relog: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Confirm: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Quit: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Netsplit: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class NewJob: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class EndJob: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Ready: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Proc: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Calc: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class Result: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+
+class CompilFail: public ANetworkPacket
+{
+	public:
+		static ANetworkPacket *create(FILE * socket);
+		std::stringstream *getStream(size_t *buflen) const;
+};
+}
+}
