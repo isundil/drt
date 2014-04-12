f23d6874c1d00fcecd3219018f2ba22986e632cb
combes_v <valentin.combes@gmail.com>
2014-04-12 13:10:49 +0300

    dans le precedent push j'ai test advanced_light en utilisant basic_light et j'ai oublie de remettre Spot de basic_light

--- server/src/modules/basic_light/Spot.cpp
+++ server/src/modules/basic_light/Spot.cpp
@@ -4,11 +4,9 @@
 #include "Scene.hpp"
 
 Spot::Spot() {
-  std::cout << "Spot initialized" << std::endl;
-  x = 0;
-  y = 0;
-  z = 0;
-  color = 0xFFFFFF;
+  x = 0.f;
+  y = 0.f;
+  z = 0.f;
 }
 
 void		Spot::normalize(t_pt *a) {
@@ -23,8 +21,13 @@ unsigned int	Spot::applyLight(double cosa, unsigned int color) {
   unsigned int	r;
   unsigned int	g;
   unsigned int	b;
+  unsigned int	tmpcolor = color;
 
-  colorSeparator(&b, &g, &r, color);
+  b = tmpcolor & 0x0000FF;
+  tmpcolor = tmpcolor >> 8;
+  g = tmpcolor & 0x0000FF;
+  tmpcolor = tmpcolor >> 8;
+  r = tmpcolor & 0x0000FF;
 
   r = cosa * r;
   if (r > 255)
@@ -36,100 +39,13 @@ unsigned int	Spot::applyLight(double cosa, unsigned int color) {
   if (b > 255)
     b = 255;
 
-  color = colorUnificator(r, g, b);
-  return color;
+  tmpcolor = r;
+  tmpcolor = tmpcolor << 8;
+  tmpcolor += g;
+  tmpcolor = tmpcolor << 8;
+  tmpcolor += b;
+  return tmpcolor;
 } 
-
-void		Spot::colorSeparator(unsigned int *blue, unsigned int *green,
-				     unsigned int *red, unsigned int a)
-{
-  *blue = a & 0x0000FF;
-  a = a >> 8;
-  *green = a & 0x0000FF;
-  a = a >> 8;
-  *red = a & 0x0000FF;
-
-}
-
-unsigned int	Spot::colorUnificator(unsigned int red, unsigned int green, unsigned int blue)
-{
-  int	c;
-
-  c = 0;
-  c = c >> 16;
-  c = red;
-  c = c << 8;
-  c += green;
-  c = c << 8;
-  c += blue;
-  return (c);
-}
-
-unsigned int	Spot::mergeColors(unsigned int color1, unsigned int color2, unsigned int max)
-{
-  unsigned int	r1 = 0;
-  unsigned int	g1 = 0;
-  unsigned int	b1 = 0;
-  unsigned int	r2 = 0;
-  unsigned int	g2 = 0;
-  unsigned int	b2 = 0;
-  unsigned int	rm = 0;
-  unsigned int	gm = 0;
-  unsigned int	bm = 0;
-
-  colorSeparator(&b1, &g1, &r1, color1);
-  colorSeparator(&b2, &g2, &r2, color2);
-  colorSeparator(&bm, &gm, &rm, max);
-
-  // r1 = r1 * coef + r2 * (1 - coef);
-  // if (r1 > 255)
-  //   r1 = 255;
-  // g1 = g1 * coef + g2 * (1 - coef);
-  // if (g1 > 255)
-  //   g1 = 255;
-  // b1 = b1 * coef + b2 * (1 - coef);
-  // if (b1 > 255)
-  //   b1 = 255;
-
-  r1 = r1 + r2;
-  if (r1 > rm)
-    r1 = rm;
-  g1 = g1 + g2;
-  if (g1 > gm)
-    g1 = gm;
-  b1 = b1 + b2;
-  if (b1 > bm)
-    b1 = bm;
-
-  return colorUnificator(r1, g1, b1);
-}
-
-bool		Spot::isInShadow(std::map<unsigned int, drt::render::Scene::t_Item *> objects, t_pt p,
-				 t_pt l, drt::render::Scene::t_Item *obj)
-{
-  p.x += (*obj->subItems.cbegin())->object->getX();
-  p.y += (*obj->subItems.cbegin())->object->getY();
-  p.z += (*obj->subItems.cbegin())->object->getZ();
-  Camera	camera(p.x, p.y, p.z);
-  Ray		ray(l.x, l.y, l.z);
-  double	k = -1;
-  bool		res = false;
-
-  for (auto i = objects.cbegin(); i != objects.cend(); i++)
-    {
-      camera.reset();
-      ray.reset();
-      if ((*i).second->object != obj->object)
-      	{
-      	  for (auto a = (*i).second->subItems.cbegin(); a != (*i).second->subItems.cend(); a++)
-	    (*a)->object->preProcess(&camera, &ray);
-      	  k = (*i).second->object->computeEquation(&camera, &ray);
-      	  if (k < 1 && k > 0)
-	    res = true;
-	}
-    }
-  return res;
-}
 
 void		Spot::applyRotation(t_pt *n, AObject *rot)
 {
@@ -179,8 +95,6 @@ unsigned int	Spot::postProcess(drt::render::Scene * scene, Camera * camera, Ray 
   drt::render::Scene::t_Item	*lastFound = nullptr;
   drt::render::Scene::t_Item	*light = nullptr;
   unsigned int	tmpcolor;
-  unsigned int	nbSpots = 0;
-  bool		shadow = false;
   AObject *objTrans = nullptr;
   AObject *objRot = nullptr;
 
@@ -225,27 +139,18 @@ unsigned int	Spot::postProcess(drt::render::Scene * scene, Camera * camera, Ray 
 
   applyRotation(&p, objRot);
   n = obj->getNormale(p, l);
-
-  l.x = x - p.x - objTrans->getX();
-  l.y = y - p.y - objTrans->getY();
-  l.z = z - p.z - objTrans->getZ();
-
-  shadow = isInShadow(objects, p, l, lastFound);
   p.x = p.x + objTrans->getX();
   p.y = p.y + objTrans->getY();
   p.z = p.z + objTrans->getZ();
   l.x = x - p.x;
   l.y = y - p.y;
   l.z = z - p.z;
+
   this->normalize(&n);
   this->normalize(&l);
   cosa = (n.x * l.x) + (n.y * l.y) + (n.z * l.z);
   if (cosa < 0)
     cosa = 0;
-  if (shadow == false)
-    tmpcolor = applyLight(cosa, tmpcolor);
-  else
-    tmpcolor = 0;
-  color = mergeColors(tmpcolor, color, 0xFFFFFF);
-  return color;
+  tmpcolor = applyLight(cosa, tmpcolor);
+  return tmpcolor;
 }
--- server/src/modules/basic_light/Spot.hpp
+++ server/src/modules/basic_light/Spot.hpp
@@ -17,16 +17,12 @@ public:
   virtual double	computeEquation(Camera * camera, Ray * ray) {return -1;}
   virtual void		preProcess(Camera * camera, Ray * ray) {}
   virtual unsigned int	postProcess(drt::render::Scene *, Camera *, Ray *, AObject *, double, unsigned int);
-  virtual unsigned int	getColor() {return color;}
+  virtual unsigned int	getColor() {return 0;}
   virtual t_pt		getNormale(t_pt, t_pt) {t_pt t; return t;}
   virtual double	getX() {return x;}
   virtual double	getY() {return y;}
   virtual double	getZ() {return z;}
-  void			colorSeparator(unsigned int *, unsigned int *, unsigned int *, unsigned int);
-  unsigned int		colorUnificator(unsigned int red, unsigned int green, unsigned int blue);
-  unsigned int		mergeColors(unsigned int color1, unsigned int color2, unsigned int max);
   unsigned int		applyLight(double, unsigned int);
-  bool			isInShadow(std::map<unsigned int, drt::render::Scene::t_Item *>, t_pt, t_pt, drt::render::Scene::t_Item *);
   void			normalize(t_pt *);
   void			applyRotation(t_pt *n, AObject *rot);
 
@@ -34,5 +30,4 @@ private:
   double	x;
   double	y;
   double	z;
-  unsigned int	color;
 };
