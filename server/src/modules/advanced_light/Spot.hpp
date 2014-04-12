6634131e0a6e5e800410b9fcf782efd41e1aeefe
combes_v <valentin.combes@gmail.com>
2014-04-12 16:53:03 +0300

    patch advanced_light

--- server/src/modules/advanced_light/Spot.hpp
+++ server/src/modules/advanced_light/Spot.hpp
@@ -16,7 +16,7 @@ public:
 
   virtual double	computeEquation(Camera * camera, Ray * ray) {return -1;}
   virtual void		preProcess(Camera * camera, Ray * ray) {}
-  virtual unsigned int	postProcess(drt::render::Scene *, Camera *, Ray *, AObject *, double, unsigned int, unsigned int, unsigned int);
+  virtual unsigned int	postProcess(drt::render::Scene *, Camera *, Ray *, AObject *, double, unsigned int);
   virtual unsigned int	getColor() {return color;}
   virtual t_pt		getNormale(t_pt, t_pt) {t_pt t; return t;}
   virtual double	getX() {return x;}
---
+++ server/src/modules/advanced_light/Transparency.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "AObject.hpp"
+
+class	Camera;
+class	Ray;
+class	Scene;
+
+class		Transparency : public AObject {
+public:
+
+  Transparency(double c);
+  Transparency(Transparency &);
+
+  virtual double	computeEquation(Camera * camera, Ray * ray) {return -1;}
+  virtual void		preProcess(Camera * camera, Ray * ray) {}
+  virtual unsigned int	postProcess(drt::render::Scene *, Camera *, Ray *, AObject *, double, unsigned int color) {return color;}
+  virtual unsigned int	getColor() {return 0;}
+  virtual t_pt		getNormale(t_pt, t_pt) {t_pt t; return t;}
+  virtual double	getX() {return 0;}
+  virtual double	getY() {return 0;}
+  virtual double	getZ() {return 0;}
+  // virtual AObject	*clone() {return new Transparency(*this);}
+  double		getCoef();
+
+private:
+  double		coef;
+};
